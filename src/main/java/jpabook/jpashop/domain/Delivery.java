271f7d2ebaee520f1e5232123692e039b355ad26
LeverUp-Dev <123243889+LeverUp-Dev@users.noreply.github.com>
2024-01-18 01:31:13 +0900

    엔티티 클래스 개발-완

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -3,7 +3,8 @@ package jpabook.jpashop.domain;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.aspectj.weaver.ast.Or;
+
+import static jakarta.persistence.FetchType.*;
 
 @Entity
 @Getter @Setter
@@ -13,7 +14,7 @@ public class Delivery {
     @Column(name = "delivery_id")
     private Long id;
 
-    @OneToOne(mappedBy = "delivery")
+    @OneToOne(mappedBy = "delivery", fetch = LAZY)
     private Order order;
 
     @Embedded
